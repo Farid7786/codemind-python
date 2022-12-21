4705c4137edb8e5896e8466896de965a6035b42a
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-12-21 10:19:16 +0530

    ch=int(input()) l=65+ch k=[] for i in range(65,l):     k.append(chr(i)) k=k[::-1] for i in k:     for j in range(0,ch):         print(i,end=' ')     ch=ch-1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,11 +1,11 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if j==n or j==(n-1):
-            continue
-        else:
-            print(j,end="")
-    for k in range((n-3),0,-1):
-        print(k,end="")
-    print(end="
-")    
+ch=int(input())
+l=65+ch
+k=[]
+for i in range(65,l):
+    k.append(chr(i))
+k=k[::-1]
+for i in k:
+    for j in range(0,ch):
+        print(i,end=' ')
+    ch=ch-1
+    print()
